45ea7c4565ca259dc1309cc6c5fa100214dfab99
Ayan Sikdar <sikdarayan27@gmail.com>
2023-03-06 00:46:42 +0530

    New add.c file is added in this branch

---
+++ rawcodedirectory/add.c
@@ -0,0 +1,19 @@
+//Add two numbers in C . language
+#include <stdio.h>
+int main() {
+int num1, num2, sum;
+ 
+printf("Enter two numbers: ");
+scanf("%d %d", &num1, &num2);
+
+// total 2 so
+sum = num1 + num2;
+ 
+printf("The sum of 2 %d and %d is %d", num1, num2, sum);
+printf("\n------------------------------\n");
+
+
+
+
+
+
